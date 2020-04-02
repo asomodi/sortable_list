8e73ff2108b6cd1a26308b09b2ca8a1c90ea07c2
Akos Somodi <akossomodi@yahoo.com>
2020-04-02 14:21:20 +0200

    add swap list items function

--- script.js
+++ script.js
@@ -74,6 +74,17 @@ function dragDrop() {
   console.log('Event: ', 'drop');
   const dragEndIndex = +this.getAttribute('data-index');
   swapItems(dragStartIndex, dragEndIndex);
+
+  this.classList.remove('over');
+}
+
+// Swap list items that are drag and drop
+function swapItems(fromIndex, toIndex) {
+  const itemOne = listItems[fromIndex].querySelector('.draggable');
+  const itemTwo = listItems[toIndex].querySelector('.draggable');
+
+  listItems[fromIndex].appendChild(itemTwo);
+  listItems[toIndex].appendChild(itemOne);
 }
 
 function addEventListeners() {
